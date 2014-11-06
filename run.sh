9a4b8053c12471f730b461e9b481fe37ed7d2f8e
moltar <moltar@moltar.net>
2014-11-06 16:08:10 -0500

    Dont actually need sudo for plenv install!

--- run.sh
+++ run.sh
@@ -12,8 +12,6 @@ fi
 WERCKER_CACHE_DIR_PLENV="$WERCKER_CACHE_DIR/plenv"
 PLENV_ROOT=${PLENV_ROOT:-$WERCKER_CACHE_DIR_PLENV}
 
-debug "PLENV_ROOT: $PLENV_ROOT"
-
 PLENV_INSTALL_ARGS="$WERCKER_PLENV_VERSION $WERCKER_PLENV_SWITCHES"
 PLENV_VERSION_DIR=$WERCKER_PLENV_VERSION
 if [ ! -n "$WERCKER_PLENV_AS" ]; then
@@ -26,12 +24,10 @@ if [ ! -d "$PLENV_ROOT" ]; then
     info "Installing plenv" && \
     sudo apt-get update -qq && \
     sudo apt-get install -qq -y perl-modules && \
-    sudo mkdir -p $PLENV_ROOT/plugins/perl-build && \
-    curl -L --silent https://github.com/tokuhirom/plenv/archive/2.1.1.tar.gz     | sudo tar -xz --strip 1 -C $PLENV_ROOT && \
-    curl -L --silent https://github.com/tokuhirom/Perl-Build/archive/1.10.tar.gz | sudo tar -xz --strip 1 -C $PLENV_ROOT/plugins/perl-build
+    mkdir -p $PLENV_ROOT/plugins/perl-build && \
+    curl -L --silent https://github.com/tokuhirom/plenv/archive/2.1.1.tar.gz     | tar -xz --strip 1 -C $PLENV_ROOT && \
+    curl -L --silent https://github.com/tokuhirom/Perl-Build/archive/1.10.tar.gz | tar -xz --strip 1 -C $PLENV_ROOT/plugins/perl-build
 fi
-
-ls -lA $PLENV_ROOT
 
 export PATH="$PLENV_ROOT/bin:$PATH"
 eval "$(plenv init -)"
@@ -39,11 +35,11 @@ eval "$(plenv init -)"
 ## install Perl if not yet installed
 if [ ! -d "$PLENV_ROOT/versions/$PLENV_VERSION_DIR" ]; then
     info "Installing Perl $WERCKER_PLENV_VERSION" && \
-    sudo plenv install $PLENV_INSTALL_ARGS && \
+    plenv install $PLENV_INSTALL_ARGS && \
     success "Installed Perl $WERCKER_PLENV_VERSION" && \
-    sudo plenv install-cpanm && \
-    sudo cpanm Carton && \
-    sudo plenv rehash
+    plenv install-cpanm && \
+    cpanm Carton && \
+    plenv rehash
 fi
 
 apt-get clean && \
