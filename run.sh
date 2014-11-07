fd886107e3432abed38419a85376b7bf8c872468
moltar <moltar@moltar.net>
2014-11-06 21:20:15 -0500

    added debug.

--- run.sh
+++ run.sh
@@ -29,8 +29,11 @@ if [ ! -d "$PLENV_ROOT" ]; then
     curl -L --silent https://github.com/tokuhirom/Perl-Build/archive/1.10.tar.gz | tar -xz --strip 1 -C $PLENV_ROOT/plugins/perl-build
 fi
 
+debug "after plenv install"
 export PATH="$PLENV_ROOT/bin:$PATH"
+debug "after export path $PATH"
 eval "$(plenv init -)"
+debug "after eval"
 
 ## install Perl if not yet installed
 if [ ! -d "$PLENV_ROOT/versions/$PLENV_VERSION_DIR" ]; then
